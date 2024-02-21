cfb0a250940e2ea2ceec00ce1bbfc0137b208e18
Ahammed Ashraf <ahammedashraf548387@gmail.com>
2024-02-21 11:35:39 +0530

    completed code from employee management

---
+++ employeeManagement/src/main/java/com/example/employeeManagement/DTO/DepartmentDTO.java
@@ -0,0 +1,2 @@
+package com.example.employeeManagement.DTO;public class DepartmentDTO {
+}
---
+++ employeeManagement/src/main/java/com/example/employeeManagement/exception/ResourceNotFoundException.java
@@ -0,0 +1,2 @@
+package com.example.employeeManagement.exception;public class ResourceNotFoundException {
+}
